eade59c0b76d35901f4a21d7b140ad0269dd3936
a-wx83 <a-wx83@DVGSSAPPJB001UK.dev.global.tesco.org>
2019-08-25 08:26:15 +0100

    add proj view controller

--- src/main/java/com/solvesocials/assignmentcorner/MainController.java
+++ src/main/java/com/solvesocials/assignmentcorner/MainController.java
@@ -1,27 +1,38 @@
 package com.solvesocials.assignmentcorner;
 
-import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.io.PrintWriter;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.Base64;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class MainController {
-	
+
 	@RequestMapping("/assignmentcorner/persistfile")
-	public void persistFile(@RequestParam(name = "fileData") String base64String) throws FileNotFoundException {
-		System.out.println("called");
+	public void persistFile(@RequestParam(name = "fileData") String base64String) throws IOException {
 		byte[] decoded = Base64.getDecoder().decode(base64String);
-		String decodedString = new String(decoded); 
+		String decodedString = new String(decoded);
 		System.out.println("decoded string is: " + decodedString);
-		String fileName = "/home/hrgprasad/assignmentRequests/" + System.currentTimeMillis() + ".txn"; 
-		PrintWriter out = new PrintWriter(fileName);
-		out.write(decodedString);
-		out.flush(); 
-		out.close();
+
+		String fileName = "projects.txn";
+		FileWriter fileWriter = new FileWriter(fileName, true);
+		PrintWriter printWriter = new PrintWriter(fileWriter);
+		printWriter.println(decodedString);
+		printWriter.close();
+	}
+
+	@RequestMapping("/assignmentcorner/getProjects")
+	@ResponseBody
+	public String showProjects() throws IOException {
+		byte[] encoded = Files.readAllBytes(Paths.get("projects.txn"));
+		return new String(encoded);
 	}
 
 }
